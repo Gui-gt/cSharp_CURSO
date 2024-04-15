461a340e18c11087c24cf1b8317d5387f39318c8
Gui-gt <30755guilherme@gmail.com>
2024-04-15 20:59:03 -0300

    att

---
+++ aula01/aula01.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace csharp_CURSO
+{
+    public class aula01
+    {
+        static void Main (string[] args) 
+        {
+            Console.Write("Hello Word");
+        }
+    }
+}
