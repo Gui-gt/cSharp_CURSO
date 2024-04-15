d4d26dc6de97f2959b6e98c64abe3ab3f03e5886
Gui-gt <30755guilherme@gmail.com>
2024-04-14 21:15:35 -0300

    att

--- index.cs
+++ index.cs
@@ -1,1 +1,7 @@
-using System; //comando using, indicando que quero usar a biblioteca sistema que possui os comandos basicos de entrada e saida, para impressao, leitura do teclado, console, gerenciamento de variaveis
+using System; //comando using, indicando que quero usar a biblioteca sistema que possui os comandos basicos de entrada e saida, para impressao, leitura do teclado, console, gerenciamento de variaveis(comandos basicos estao nessa biblioteca), possoo ate criar minhas bibliotecas e existem muitas outras bibliotecas que vamos usar;;
+
+class Principal{
+    static void Main(){
+        Console.Write("Olá mundo");
+    }
+}
