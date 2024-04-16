671f8c5fb3cd69c389840a133b25bd45f9100634
Gui-gt <30755guilherme@gmail.com>
2024-04-16 18:50:25 -0300

    att

---
+++ aula02/aula02.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace csharp_CURSO
+{
+    public class aula02
+    {
+
+        int num = 5;//ESSE "METODO" É GLOBAL E SERVE PRA CLASS INTEIRA 
+
+        static void Main (string[] args) 
+        {
+            int num = 1; //esse só pertence a esse metodo Main
+            Console.Write(num);
+        }
+
+        void Principal ()
+        {
+            int num = 2;//E esse só a Principal
+        }
+    }
+}
