36d2cc7a0b3dc7f367538f4796b9c7cb1e1f36a7
Gui-gt <30755guilherme@gmail.com>
2024-04-16 20:59:20 -0300

    att

---
+++ aula05/aula05.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace aula05
+{
+    public class aula05
+    {
+        static void Main ()
+        {
+            int num = (10) * 2;//Exemplo de precedente
+            bool = >= <= != ;//Tipos de operadores(igual, maior ou igual, menor ou igual e diferente)
+        }
+    }
+}
