969ef5cb355a5e66392493417fb19b05ab615dee
Gui-gt <30755guilherme@gmail.com>
2024-04-16 21:44:43 -0300

    att

--- aula06/aula06.cs
+++ aula06/aula06.cs
@@ -11,4 +11,25 @@ namespace CSHARP_CURSO
             Console.Write("n1={0},n2={1},n3={2}"n1,n2,n3);
         }
     }
+}using System;
+
+namespace CSHARP_CURSO
+{
+    public class Program
+    {
+        static void Main ()
+        {
+            double valorCompra = 5.50;
+            double venda;
+            double lucro=0.1;
+            string produto = "Pastel";
+
+            venda = valorCompra+(valorCompra*lucro);
+
+            Console.Write("Produto.....{0,15}",produto);
+            Console.Write("Val.compra.....{0,15:c}",valorCompra);//formatando valores/operações
+            Console.Write("Lucro.....{0,15:p}",lucro);
+            Console.Write("Val.Venda.....{0,15:c}",venda);
+        }
+    }
 }
