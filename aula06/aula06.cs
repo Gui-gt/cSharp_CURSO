a464a7bee8ef3da23f36a7f986aef9834c198ffb
Gui-gt <30755guilherme@gmail.com>
2024-04-16 21:22:57 -0300

    att

---
+++ aula06/aula06.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CSHARP_CURSO
+{
+    public class aula06
+    {
+        static void Main ()
+        {
+            int n1=10, n2=20, n3=30;
+
+            Console.Write("n1={0},n2={1},n3={2}"n1,n2,n3);
+        }
+    }
+}
