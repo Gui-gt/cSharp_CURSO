83c75c2c44656f6ecc88e3844ddec2d7821127d2
Gui-gt <30755guilherme@gmail.com>
2024-04-16 21:10:45 -0300

    att

--- aula05/aula05.cs
+++ aula05/aula05.cs
@@ -8,6 +8,12 @@ namespace aula05
         {
             int num = (10) * 2;//Exemplo de precedente
             bool = >= <= != ;//Tipos de operadores(igual, maior ou igual, menor ou igual e diferente)
+
+            bool = (5>3)|(10<5);//tem que dar TRUE
+            //&& AND / E
+            //| OR / OU             /OPERADORES LOGICOS
+
+
         }
     }
 }
