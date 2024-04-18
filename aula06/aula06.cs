945065e0d815c4139f509a00c08f157b203a7549
Gui-gt <30755guilherme@gmail.com>
2024-04-18 19:16:15 -0300

    Update aula06.cs

--- aula06/aula06.cs
+++ aula06/aula06.cs
@@ -469,3 +469,67 @@ namespace program
         }
     }
 }
+using System;
+using System.Diagnostics;
+
+namespace program
+{
+    public class Program
+    {
+        static void Main () {
+            int tempo = 0;
+            char escolha;
+
+
+            inicio:
+
+            Console.Clear();
+
+            Console.Write("Viagem para Belo Horizonte/MG a Vitoria/ES");
+            Console.Write("Escolha o meio de trasnporte: [A] AVIÃO  [C] CARRO  [O] ONIBUS");
+            escolha = char.Parse(Console.ReadLine());
+            switch(escolha) 
+            {
+                case 'a':
+                case 'A':
+                tempo=50;
+                break;
+
+                case 'c':
+                case 'C':
+                tempo=480;
+                break;
+
+                case 'o':
+                case 'O':
+                tempo=660;
+                break;
+
+                default:
+
+                tempo=-1;
+
+                break;
+
+            }
+
+            if(tempo < 0){
+                Console.Write("Transporte indisponivel");
+            }else{
+                Console.Write("O tempo do transporte escolhido é de {0}",tempo);
+            }
+
+            Console.WriteLine("\nCalcular outro transporte?[S/N]");
+            escolha=char.Parse(Console.ReadLine());
+
+                if(escolha == 's' || escolha == 'S'){
+                    goto inicio;
+                }
+                else{
+                    Console.Clear();
+                    Console.Write("FIM DO PROGRAMA");
+                }
+
+        }
+    }
+}
