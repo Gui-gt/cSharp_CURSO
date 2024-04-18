8929fb61288a0f29448da5c7eca4ee5443bbdeb9
Gui-gt <30755guilherme@gmail.com>
2024-04-17 21:02:08 -0300

    ATT

--- aula06/aula06.cs
+++ aula06/aula06.cs
@@ -163,3 +163,15 @@ class Program
         Console.Write(n2);
     }
 }
+using System;
+
+class Program
+{
+    static void Main ()
+    {
+        float n1 = 10.5f;
+        int n2 = (int)n1;//Conversão typecast
+
+        Console.Write(n2);
+    }
+}
