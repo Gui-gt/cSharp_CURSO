b02fa02279446121ece2b96761488351194cd895
Gui-gt <30755guilherme@gmail.com>
2024-04-20 18:11:19 -0300

    attt

--- aula06/aula06.cs
+++ aula06/aula06.cs
@@ -672,3 +672,70 @@ namespace Cadastro.Elo.Csharp
         }
     }
 }
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Cadastro.Elo.Csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(MySqlConnection conexão = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=base_pessoas;User=root;Password="))
+            {
+                conexão.Open();
+
+                using (MySqlCommand cmd = conexão.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO pessoas (nome,documento,endereco) VALUES (@nome,@documento,@endereco)";
+                    cmd.Parameters.AddWithValue("@nome", Txtnome.Text);
+                    cmd.Parameters.AddWithValue("@documento", Txtcpf.Text);
+                    cmd.Parameters.AddWithValue("@endereco", Txtendereco.Text);
+                    
+                  
+                    cmd.ExecuteNonQuery();
+                }
+
+                    MessageBox.Show("gg");
+            }
+        }
+    }
+}
