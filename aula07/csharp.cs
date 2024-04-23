d9ffb537be127ad75c600939790dc84a6a7429ab
Gui-gt <30755guilherme@gmail.com>
2024-04-23 20:58:00 -0300

    aa

--- aula07/csharp.cs
+++ aula07/csharp.cs
@@ -174,3 +174,9 @@ namespace program
         }
     }
 }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
