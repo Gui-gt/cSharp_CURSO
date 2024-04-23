5f5b2a9bc82a96a3c39be77df1b2769a90d0a1c4
Gui-gt <30755guilherme@gmail.com>
2024-04-22 21:28:46 -0300

    delete

--- obj/Debug/net7.0/cSharp_CURSO.AssemblyInfo.cs
+++ obj/Debug/net7.0/cSharp_CURSO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca640c6c489ba6cd924bb0414da28cdd7dd2525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02fa02279446121ece2b96761488351194cd895")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
