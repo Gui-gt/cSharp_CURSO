9020135d24611e0e14ce5cf573751708321563e7
Gui-gt <30755guilherme@gmail.com>
2024-04-22 22:51:44 -0300

    deu certo

--- aula07/csharp.cs
+++ aula07/csharp.cs
@@ -60,3 +60,60 @@ namespace program//nome do diretorio
         }
     }
 }
+using System;
+
+namespace program
+{
+    public class Program
+    {
+        static void Main ()
+        {
+            int tempo = 0;
+            char escolha;
+
+            inicio:
+            Console.Clear();
+
+            Console.Write("Viagem de Londrina para Maringá");
+            Console.Write("Escolha seu meio de trasnporte: A) Carro B) Moto");
+            escolha=char.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 'a':
+                case 'A':
+                tempo = 50;
+                break;
+
+                case 'b':
+                case 'B':
+                tempo = 40;
+                break;
+
+                default:
+
+                tempo =-1;
+
+                break;
+            }
+
+                if(tempo < 0){
+                    Console.Write("Transporte indisponivel");
+                }
+                else{
+                    Console.Write("O transporte escolhido levara {0} minutos pra chegar",tempo);
+                }
+
+                Console.Write("\nContinuar escolhendo? N/S");
+                escolha=char.Parse(Console.ReadLine());
+
+                if(escolha == 's' || escolha == 'S'){
+                    goto inicio;
+                }else{
+                    Console.Clear();
+                    Console.Write("Fim das escolhas!");
+                }
+
+        }
+    }
+}
