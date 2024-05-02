00de76ab1a8e52706482be78543963cc1e3df981
Gui-gt <30755guilherme@gmail.com>
2024-05-02 19:07:21 -0300

    att

--- obj/Debug/net7.0/cSharp_CURSO.AssemblyInfo.cs
+++ obj/Debug/net7.0/cSharp_CURSO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa238f0c818437ed7b9b3a9852890b6f664fcc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29dbd2cbdf537d5efbad2db8b7d85ca729a5ad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
