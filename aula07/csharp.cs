c434cd7519f3e7091f13f04201665ff834d2b62e
Gui-gt <30755guilherme@gmail.com>
2024-05-10 18:47:31 -0300

    Update csharp.cs

--- aula07/csharp.cs
+++ aula07/csharp.cs
@@ -1,1 +1,50 @@
-using S
+using Susing System;
+
+namespace program
+{
+    public class Program
+    {
+            static void Main ()
+            {
+                int tempo = 0;
+                char escolha;
+
+                inicio:
+                Console.WriteLine("Viagem de rolandia para londrina:");
+                Console.WriteLine("ESCOLHA SEU MEIO DE TRANSPORTE: A) CARRO B) MOTO");
+                escolha = char.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+
+                    case 'a':
+                    case 'A':
+                    tempo = 30;
+                    break;
+
+                    case 'b':
+                    case 'B':
+                    tempo = 20;
+                    break;
+
+
+
+                    
+                    default:
+
+                    tempo = -1;
+
+                    break;
+                }
+
+                if(tempo == 30){
+                    Console.WriteLine("VC ESCOLHEU IR DE CARRO, 30 MNTS");
+                }
+                else if(tempo == 20){
+                    Console.WriteLine("VC ESCOLHEU IR DE MOTO, LEVARA 20 MNTS");
+                }
+
+                goto inicio;
+            }
+    }
+}
