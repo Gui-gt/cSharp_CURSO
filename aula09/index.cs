f3d02f430a7b12c6df93dac2bd88a74ebcf6be2b
Gui-gt <30755guilherme@gmail.com>
2024-05-19 19:58:19 -0300

    estudando mais c#
    foco em c#, metodos e inteiros

---
+++ aula09/index.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CSHARP_CURSO
+{
+    public class index
+    {
+        static void Main()
+        {
+            cfb();
+        }
+
+        static void cfb()
+        {
+            Console.WriteLine("OLA MUNDO");
+        }
+    }
+}
--- obj/Debug/net7.0/cSharp_CURSO.AssemblyInfo.cs
+++ obj/Debug/net7.0/cSharp_CURSO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0eadbf1d1c52febef9aa7e56157cc39316f5f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036d07e88b7d23956eac21c83e93115fc1f171b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharp_CURSO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
