f412bc0d6702e9fcd33f0b1567a77d5fc9dcc7aa
Gui-gt <30755guilherme@gmail.com>
2024-05-26 16:50:23 -0300

    Update index.cs

--- index.cs
+++ index.cs
@@ -1,4 +1,4 @@
-using System; //comando using, indicando que quero usar a biblioteca sistema que possui os comandos basicos de entrada e saida, para impressao, leitura do teclado, console, gerenciamento de variaveis(comandos basicos estao nessa biblioteca), possoo ate criar minhas bibliotecas e existem muitas outras bibliotecas que vamos usar;;
+using System; //comando using, indicando que quero usar a biblioteca sistema que possui os comandos basicos de entrada e saida, para impressao, leitura do teclado, console, gerenciamento de variaveis(comandos basicos estao nessa biblioteca), possoo ate criar minhas bibliotecas e existem muitas outras bibliotecas que vamos usar
 
 class Principal{
     static void Main(){
